d78f64ed740ebb98e2483054557d873c8496c9ec
Lucas <lucasoliveirars22@gmail.com>
2023-03-16 21:09:35 -0300

    project base

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 
 import logo from 'public/img/logo.png'
 
-import styles from '../styles/index.module.css'
+import styles from '../styles/info.module.css'
 
-import imageMain from 'public/img/image-main-1.png'
+import imageAside1 from 'public/img/image-readmore-1.png'
 
-import imageAside from 'public/img/image-main-2.png'
+import imageAside2 from 'public/img/image-readmore-2.png'
 
 import logoGithub from 'public/img/logo-github.png'
 
@@ -34,7 +34,6 @@ export default function Home() {
             </Link>
             <Link href='./articles'>
               Artigos 
-            
             </Link>
           
           </div> 
@@ -47,25 +46,23 @@ export default function Home() {
           
           <div className={styles.center}>
             
-            <p>Branquemento dos Corais</p>
+            <h1>Sobre o Evento</h1>
             
-            <h1>Estratégias para <br/> restaurar os recifes</h1>
-            
-            <Image src={imageMain} alt="corais e peixes no fundo do mar" height={350} width={650}/>
+            <p>O SOS Corais reunirá cientistas e especialistas renomados em ecologia marinha e conservação para apresentar as mais recentes descobertas sobre o branqueamento dos corais e discutir as possíveis soluções para enfrentar essa ameaça crescente.</p>
+
+            <p>Entre os tópicos abordados estarão as causas e os efeitos do branqueamento dos corais, a importância dos corais para os ecossistemas marinhos, as estratégias de conservação para proteger os recifes de coral e as implicações econômicas e sociais do declínio dos recifes de coral em todo o mundo.</p>
+
+            <p>O evento também incluirá sessões de debates e mesas redondas, permitindo que os participantes compartilhem experiências e ideias para colaborar no desenvolvimento de estratégias mais eficazes para enfrentar este desafio crescente. Além disso, os participantes terão a oportunidade de participar de workshops interativos para discutir temas específicos relacionados ao branqueamento dos corais.</p>
+
+            <p>Será uma excelente oportunidade para cientistas, especialistas e interessados em geral, para aprender mais sobre o branqueamento dos corais, trocar informações e desenvolver estratégias para enfrentar esse desafio crucial para a conservação dos oceanos e a sustentabilidade do planeta.</p>
         
           </div>
         
           <div className={styles.aside}>
           
-            <p>
-              O evento reunirá cientistas e especialistas renomados em ecologia marinha e conservação ara presentar as mais recentes descobertas sobre o branqueamento dos corais e discutir as possíveis soluções para enfrentar essa ameaça crescente.
-            </p>
-
-            <Link href='./info'>
-              <b> Ler Mais </b> 
-            </Link>
-
-            <Image src={imageAside} alt="corais e peixes no fundo do mar" height={325} width={250}/>
+            <Image src={imageAside1} alt="corais e peixes no fundo do mar" className={styles.img1} height={330} width={250}/>
+            
+            <Image src={imageAside2} alt="corais e peixes no fundo do mar" height={330} width={250}/>
           
           </div>
         
